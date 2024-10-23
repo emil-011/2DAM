6483d95c95576f782b9bb94c2fb18e475941abad
EmilioFdez12 <emiliofdezglld12@gmail.com>
2024-10-23 13:42:23 +0200

    Terminar

--- D.Interfaces/Practica2-DI/src/utils/Cliente.java
+++ D.Interfaces/Practica2-DI/src/utils/Cliente.java
@@ -6,12 +6,14 @@ public class Cliente {
 	private String apellido;
 	private int edad;
 	private String provincia;
+	private String email;
 
-	public Cliente(String nombre, String apellido, int edad, String provincia) {
+	public Cliente(String nombre, String apellido, int edad, String provincia, String email) {
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.edad = edad;
 		this.provincia = provincia;
+		this.email = email;
 	}
 
 	public String getNombre() {
@@ -29,11 +31,15 @@ public class Cliente {
 	public String getProvincia() {
 		return provincia;
 	}
+	
+	public String getEmail() {
+		return email;
+	}
 
 	@Override
 	public String toString() {
 		return "👤 Cliente: " + nombre.toUpperCase() + " " + apellido.toUpperCase() + " | Edad: " + edad
-				+ " años | Provincia: " + provincia;
+				+ " años | Provincia: " + provincia + " | Email: " + email;
 	}
 
 }
