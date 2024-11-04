01c12fef5363f6de8cbbdacbf6bb3ce5802896ae
EmilioFdez12 <emiliofdezglld12@gmail.com>
2024-11-04 20:34:41 +0100

    Look&Feel

---
+++ D.Interfaces/LookAndFeel/src/mainApp/MainApp.java
@@ -0,0 +1,13 @@
+package mainApp;
+
+import gui.MainView;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		MainView mainView = new MainView();
+		mainView.setVisible(true);
+
+	}
+
+}
--- D.Interfaces/PicassoGYM/src/main/MainApp.java
+++ D.Interfaces/PicassoGYM/src/main/MainApp.java
@@ -11,6 +11,13 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
 public class MainApp {
 
 	private static final String USUARIOS_REGISTRADOS = "usuarios_registrados.csv";
@@ -19,6 +26,12 @@ public class MainApp {
 		Usuario usuarioLogueado = checkLoggedInUser();
 
 		if (usuarioLogueado != null) {
+			try {
+				UIManager.setLookAndFeel(new FlatIntelliJLaf());
+			} catch (UnsupportedLookAndFeelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			// User is logged in, open the corresponding window based on user profile
 			String perfil = usuarioLogueado.getPerfil();
 			if (perfil.equals("cliente")) {
