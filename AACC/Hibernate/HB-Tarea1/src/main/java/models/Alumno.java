71d8d13d8fd08f282d7764804f0ca12495424fd9
EmilioFdez12 <emiliofdezglld12@gmail.com>
2024-11-07 12:48:53 +0100

    Hibernate Pruebas

---
+++ AACC/Hibernate/HB-Tarea1/src/main/java/models/Alumno.java
@@ -0,0 +1,72 @@
+package models;
+
+import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Alumno implements Serializable {
+	// Comentario mas grande
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idAlumno;
+
+	@Column
+	private String nombre;
+
+	@Column
+	private String apellido;
+
+	@Column
+	private int edad;
+
+//	@OneToOne(cascade = CascadeType.ALL)
+//	@PrimaryKeyJoinColumn
+//	private Matricula matricula; 
+
+	public Alumno() {
+		super();
+	}
+
+	public Alumno(String nombre, String apellido, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+
+	public int getIdAlumno() {
+		return idAlumno;
+	}
+
+	public void setIdAlumno(int num) {
+		this.idAlumno = num;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+}
