18e367b72a504dff2eda9a4d607e6ccde8330236
EmilioFdez12 <emiliofdezglld12@gmail.com>
2024-11-26 10:21:30 +0100

    HQL Pruebas

--- AACC/Hibernate/HB-Tarea2/src/main/java/main/MainApp.java
+++ AACC/Hibernate/HB-Tarea2/src/main/java/main/MainApp.java
@@ -1,27 +1,27 @@
 package main;
 
-import models.Existencias;
-import models.Localidad;
-import models.Titular;
-import utils.RestauranteHibernateUtil;
+import org.hibernate.query.Query;
+
+import utils.HibernateUtil;
+
+import java.util.List;
 
 public class MainApp {
 
-	public static void main(String[] args) {
-		// Insertamos localidad
-//		Localidad localidad = new Localidad(31, "Rincon");
-//		RestauranteHibernateUtil.insertarLocalidad(localidad);
-//		Restaurante restaurante = new Restaurante("R001", "Restaurante La Casa", "1234567890", "Calle Falsa 123", 
-//                Date.valueOf("2024-11-22"), "08:00 - 22:00", localidad);
-//		RestauranteHibernateUtil.insertarRestaurante(restaurante);
-//		Existencias hamburguesas = new Existencias("HAMB001", "Hamburguesas", 100, 5.99, "R001");
-//		RestauranteHibernateUtil.insertarExistencias(hamburguesas);
-		Titular titular = new Titular("11111111A","Pepin", "Calle Trebol", "R001");
-		RestauranteHibernateUtil.insertarTitular(titular);
-		
+    public static void main(String[] args) {
+        
+        String hql = "SELECT c.categoria FROM Categorias c";  // Selecciona solo la propiedad 'categoria'
+        Query<String> query = HibernateUtil.getSession().createQuery(hql, String.class);
 
-		
-		
-	}
+        // Ejecuta la consulta y obtén los resultados
+        List<String> categorias = query.list();
 
+        // Imprime los resultados
+        for (String categoria : categorias) {
+            System.out.println(categoria);
+        }
+
+        // Cierra la sesión
+        HibernateUtil.closeSession();
+    }
 }
