68671ad369631846f582097e73729210d97c573d
EmilioFdez12 <emiliofdezglld12@gmail.com>
2024-11-27 10:01:00 +0100

    Validaciones 4 donete

--- Seguridad/validaciones04/src/hlc/ud03/relacion04/Acondicionador.java
+++ Seguridad/validaciones04/src/hlc/ud03/relacion04/Acondicionador.java
@@ -1,46 +1,88 @@
 package hlc.ud03.relacion04;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class Acondicionador {
 
-    public static String acondicionaElementoHtml(String cadena) {
-        if (cadena == null || cadena.isEmpty()) {
-            return "";
-        }
-
-        // Reemplaza caracteres especiales por sus entidades HTML
-        return cadena
-                .replace("&", "&amp;")   // Ampersand
-                .replace("<", "&lt;")    // Menor que
-                .replace(">", "&gt;")    // Mayor que
-                .replace("\"", "&#34;") // Comillas dobles
-                .replace("'", "&#39;");  // Comillas simples
-    }
-
-    public static String acondicionaAtributoHtml(String cadena) {
-        if (cadena == null || cadena.isEmpty()) {
-            return "";
-        }
-
-        // Reemplaza caracteres especiales por sus entidades HTML
-        return cadena
-                .replace("&", "&amp;")   // Ampersand
-                .replace("\"", "&#34;") // Comillas dobles
-                .replace("'", "&#39;"); // Comillas simples
-    }
-
-	public static String acondicionaContenidoCss(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * 
+	 * @param cadena
+	 * @return
+	 */
+	public static String acondicionaElementoHtml(String cadena) {
+		if (cadena == null || cadena.isEmpty()) {
+			return "";
+		}
+		return cadena.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&#34;")
+				.replace("'", "&#39;");
 	}
 
-	public static String acondicionaUrl(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * 
+	 * @param cadena
+	 * @return
+	 */
+	public static String acondicionaAtributoHtml(String cadena) {
+		if (cadena == null || cadena.isEmpty()) {
+			return "";
+		}
+		return cadena.replace("&", "&amp;").replace("<", "&lt;").replace("\"", "&#34;").replace("'", "&#39;");
 	}
 
-	public static String acondicionaLiteralSql(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * 
+	 * @param cadena
+	 * @return
+	 */
+	public static String acondicionaContenidoCss(String cadena) {
+		char[] cadenaSpliteada = cadena.toCharArray();
+		StringBuilder cadenaEntera = new StringBuilder();
+
+		for (int i = 0; i < cadenaSpliteada.length; i++) {
+			char c = cadenaSpliteada[i];
+
+			if (Character.isDigit(c)) {
+				cadenaEntera.append(" ");
+			}
+
+			cadenaEntera.append(c);
+		}
+
+		cadena = cadenaEntera.toString();
+
+		return cadena.replace("\\", "\\5c").replace("\"", "\\22").replace("'", "\\27").replace("<", "\\3c")
+				.replace(">", "\\3e").replace("&", "\\26").replace("(", "\\28").replace(")", "\\29")
+				.replace("/", "\\2f");
+	}
+
+	/**
+	 * 
+	 * @param cadena
+	 * @return
+	 */
+	public static String acondicionaUrl(String cadena) {
+		try {
+			cadena = URLEncoder.encode(cadena, "UTF-8").replace("%40", "@").replace("+", "%20");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		return cadena;
+	}
+
+	/**
+	 * 
+	 * @param cadena
+	 * @return
+	 */
+	public static String acondicionaLiteralSql(String cadena) {
+		System.out.println(cadena);
+		cadena = cadena.replace("\\", "\\\\").replace("'", "\\'").replace("\"", "\\\"").replace("%", "\\%").replace("_",
+				"\\_");
+		System.out.println(cadena);
+
+		return cadena;
 	}
 
 }
--- Seguridad/validaciones04/src/hlc/ud03/relacion04/TestAcondicionadorApp.java
+++ Seguridad/validaciones04/src/hlc/ud03/relacion04/TestAcondicionadorApp.java
@@ -21,29 +21,29 @@ public class TestAcondicionadorApp {
       errores++;
     }
     
-//    if (!assertIgual(Acondicionador.acondicionaContenidoCss("Hola Caracola"), "Hola Caracola", "acondicionaContenidoCss")) {
-//      errores++;
-//    }
-//    
-//    if (!assertIgual(Acondicionador.acondicionaContenidoCss("\"1 h ' o \\ l < a & C (a )r /1 ola"), "\\22 1 h \\27 o \\5c l \\3c a \\26 C \\28 a \\29r \\2f 1 ola", "acondicionaContenidoCss")) {
-//      errores++;
-//    }
-//    
-//    if (!assertIgual(Acondicionador.acondicionaUrl("Hola Caracola"), "Hola%20Caracola", "acondicionaUrl")) {
-//      errores++;
-//    }
-//    
-//    if (!assertIgual(Acondicionador.acondicionaUrl("@\\/ Ω"), "@%5C%2F%20%CE%A9", "acondicionaUrl")) {
-//      errores++;
-//    }
-//    
-//    if (!assertIgual(Acondicionador.acondicionaLiteralSql("Hola Caracola"), "Hola Caracola", "acondicionaLiteralSql")) {
-//      errores++;
-//    }
-//    
-//    if (!assertIgual(Acondicionador.acondicionaLiteralSql("H\"'\\%_ ola"), "H\\\"\\'\\\\\\%\\_ ola", "acondicionaLiteralSql")) {
-//      errores++;
-//    }
+    if (!assertIgual(Acondicionador.acondicionaContenidoCss("Hola Caracola"), "Hola Caracola", "acondicionaContenidoCss")) {
+      errores++;
+    }
+    
+    if (!assertIgual(Acondicionador.acondicionaContenidoCss("\"1 h ' o \\ l < a & C (a )r /1 ola"), "\\22 1 h \\27 o \\5c l \\3c a \\26 C \\28a \\29r \\2f 1 ola", "acondicionaContenidoCss")) {
+      errores++;
+    }
+    
+    if (!assertIgual(Acondicionador.acondicionaUrl("Hola Caracola"), "Hola%20Caracola", "acondicionaUrl")) {
+      errores++;
+    }
+    
+    if (!assertIgual(Acondicionador.acondicionaUrl("@\\/ Ω"), "@%5C%2F%20%CE%A9", "acondicionaUrl")) {
+      errores++;
+    }
+    
+    if (!assertIgual(Acondicionador.acondicionaLiteralSql("Hola Caracola"), "Hola Caracola", "acondicionaLiteralSql")) {
+      errores++;
+    }
+    
+    if (!assertIgual(Acondicionador.acondicionaLiteralSql("H\"'\\%_ ola"), "H\\\"\\'\\\\\\%\\_ ola", "acondicionaLiteralSql")) {
+      errores++;
+    }
     
     if (errores == 0) {
       System.out.println("No se produjeron errores");
